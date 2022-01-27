d86ad4bca6c0ec34e8de16f6fdbbb7f8d39fb1cd
w-a-gomes <wildenalisson@outlook.com>
2022-01-27 11:55:25 -0300

    Examples

--- README.md
+++ README.md
@@ -41,3 +41,31 @@ fn main() {
 
 ```
 ![Image](data/screenshot_01.png "screenshot")
+
+Drawing the details
+
+```rust
+// Settings
+let width = 60;
+let height = 15;
+let char_1 = Some('+');             // Default is *
+let char_2 = Some('-');             // Default is -
+let color_1 = Some((47, 87, 124));  // Default is system/terminal color
+let color_2 = Some((29, 79, 41));   // Default is system/terminal color
+
+// Instance
+let mut monit = monitor::Monitor::new(width, height, char_1, char_2, color_1, color_2);
+
+// Show
+for _ in 0..width {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
+    let mut bitcoin_value = monitor::ValueEmulator::new(0, height);
+
+    for (n, m) in monit.status(bitcoin_value.value()).iter().enumerate() {
+        println!(" {} {}", m, n + 1);
+    }
+    println!("  {}", "----|".repeat((width/5) as usize));
+    sleep(Duration::from_millis(100));
+}
+```
+![Image](data/screenshot_02.png "screenshot")
--- examples/main.rs
+++ examples/main.rs
@@ -4,6 +4,11 @@ use std::thread::sleep;
 use monitor;
 
 fn main() {
+    simple_monitor();
+    detailed_monitor();
+}
+
+fn simple_monitor() {
     // Monitor settings
     let width = 60;
     let height = 15;
@@ -26,6 +31,31 @@ fn main() {
         for monitor_lines in monit.status(bitcoin_value.value()) {
             println!("{}", monitor_lines);
         }
-        sleep(Duration::from_millis(500)); // Slow down display
+        sleep(Duration::from_millis(100)); // Slow down display
+    }
+}
+
+fn detailed_monitor() {
+    // Settings
+    let width = 60;
+    let height = 15;
+    let char_1 = Some('+');             // Default is *
+    let char_2 = Some('-');             // Default is -
+    let color_1 = Some((47, 87, 124));  // Default is system/terminal color
+    let color_2 = Some((29, 79, 41));   // Default is system/terminal color
+
+    // Instance
+    let mut monit = monitor::Monitor::new(width, height, char_1, char_2, color_1, color_2);
+
+    // Show
+    for _ in 0..width {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
+        let mut bitcoin_value = monitor::ValueEmulator::new(0, height);
+
+        for (n, m) in monit.status(bitcoin_value.value()).iter().enumerate() {
+            println!(" {} {}", m, n + 1);
+        }
+        println!("  {}", "----|".repeat((width/5) as usize));
+        sleep(Duration::from_millis(100));
     }
 }
