3c36eb8879eaac2c34b3325075eae793481383b3
w-a-gomes <wildenalisson@outlook.com>
2022-01-30 21:54:40 -0300

    Ex

--- README.md
+++ README.md
@@ -13,7 +13,12 @@ use std::thread::sleep;
 use monitor;
 
 fn main() {
-    let mut monit = monitor::Monitor::new();  // Monitor instance
+    // Monitor instance
+    let mut monit = monitor::Monitor::new();
+
+    // Monitor settings: Char and RGB color - Default color (None), is system color
+    monit.set_primary_character(Some('-'), Some((222, 17, 64)));  // Default char is #
+    monit.set_secondary_character(None, Some((64, 38, 80)));      // Default char is -
 
     // A loop controls the monitor display time
     for _ in 0..60 {  // If 'sleep' below is 1 second, then using 60 here is 1 minute in loop
@@ -36,9 +41,10 @@ Drawing the details
 ```rust
 // Instance
 let mut monit = monitor::Monitor::new();
-// Monitor settings
-monit.set_primary_character(Some('*'), Some((47, 87, 124)));
-monit.set_secondary_character(Some('.'), Some((29, 79, 41)));
+
+// Settings
+monit.set_primary_character(Some('='), Some((30, 148, 112)));
+monit.set_secondary_character(None, Some((98, 48, 55)));
 monit.set_dimensions(70, 20);  // Width (columns) and height (lines)
 
 for _ in 0..100 {  // Display time
@@ -47,11 +53,11 @@ for _ in 0..100 {  // Display time
     // Utility to emulate real-time value:             (   MIN and MAX values  )
     let mut bitcoin_value = monitor::ValueEmulator::new(0, monit.dimensions().1);
 
-    // Show monitor row and right decoration
+    // Show monitor row and draw decoration right
     for (n, m) in monit.build_chart(bitcoin_value.value()).iter().enumerate() {
         println!(" {} {}", m, n + 1);
     }
-    // Bottom decoration
+    // Draw decoration bottom
     println!(" {}", "____!____|".repeat(7 as usize));
     let mut n = 10; print!("  "); for _ in 0..7 {print!("        {}", n); n += 10;}
 
--- examples/main.rs
+++ examples/main.rs
@@ -9,7 +9,12 @@ fn main() {
 }
 
 fn simple_monitor() {
-    let mut monit = monitor::Monitor::new();  // Monitor instance
+    // Monitor instance
+    let mut monit = monitor::Monitor::new();
+
+    // Monitor settings: Char and RGB color - Default color (None), is system color
+    monit.set_primary_character(Some('-'), Some((222, 17, 64)));  // Default char is #
+    monit.set_secondary_character(None, Some((64, 38, 80)));      // Default char is -
 
     // A loop controls the monitor display time
     for _ in 0..60 {  // If 'sleep' below is 1 second, then using 60 here is 1 minute in loop
@@ -28,9 +33,10 @@ fn simple_monitor() {
 fn detailed_monitor() {
     // Instance
     let mut monit = monitor::Monitor::new();
-    // Monitor settings
-    monit.set_primary_character(Some('*'), Some((47, 87, 124)));
-    monit.set_secondary_character(Some('.'), Some((29, 79, 41)));
+
+    // Settings
+    monit.set_primary_character(Some('='), Some((30, 148, 112)));
+    monit.set_secondary_character(None, Some((98, 48, 55)));
     monit.set_dimensions(70, 20);  // Width (columns) and height (lines)
 
     for _ in 0..100 {  // Display time
@@ -39,11 +45,11 @@ fn detailed_monitor() {
         // Utility to emulate real-time value:             (   MIN and MAX values  )
         let mut bitcoin_value = monitor::ValueEmulator::new(0, monit.dimensions().1);
 
-        // Show monitor row and right decoration
+        // Show monitor row and draw decoration right
         for (n, m) in monit.build_chart(bitcoin_value.value()).iter().enumerate() {
             println!(" {} {}", m, n + 1);
         }
-        // Bottom decoration
+        // Draw decoration bottom
         println!(" {}", "____!____|".repeat(7 as usize));
         let mut n = 10; print!("  "); for _ in 0..7 {print!("        {}", n); n += 10;}
 
